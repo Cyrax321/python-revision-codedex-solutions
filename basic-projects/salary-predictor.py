fb887372fff98933ed46845b6a55cc298d2d00e6
Cyrax321 <cyrax8590@gmail.com>
2025-06-17 15:30:03 +0530

    New salary predictor

---
+++ basic-projects/salary-predictor.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your monthly salary in USD: "))
+b = a*12
+i = a*86
+# Assuming 1 USD = 86 INR (as per the example)
+c = b*86
+print("your monthy salary in INR is: ",i,'INR')
+print("Your annual salary is: ",b,"USD","which is equivalent to",c,"INR") 
+print("Damn! that's some crazy package 🥵🔥")
+print("Lets go grinding.... NOW!!💨")
